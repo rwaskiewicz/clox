9516262c4bd13d2434efa5ec8b51392a0102cba0
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2020-12-28 10:32:52 -0500

    Thru 17.2 'Parsing Tokens'
    - Update vm.c interpret() to assign vm.chunk, vm.ip on successful compilation, then run() the code
        - Opens the proverbial doors again to gettng a chunk(s) into the vm
    - Update compile() signature to return a bool and accept a chunk as an argument
    - Update compiler.c
        - Add consume(), advance() functions for moving through/invoking scanning of tokens
        - Add error handling helpers for previous, current tokens
        - Add parser struct holding error sentinels, previous and current tokens
        - Remove temporary compile() implementation

--- compiler.c
+++ compiler.c
@@ -1,26 +1,79 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
 #include "scanner.h"
 
-void compile(const char* source) {
-  initScanner(source);
-  // temporary code to kick the cmopiler into action
-  int line = -1;
+typedef struct {
+  Token current;
+  Token previous;
+  bool hadError;
+  bool panicMode;
+} Parser;
+
+Parser parser;
+
+static void errorAt(Token* token, const char* message) {
+  if (parser.panicMode) {
+    return;
+  }
+
+  parser.panicMode = true;
+
+  fprintf(stderr, "[line %d] Error", token->line);
+
+  if (token->type == EOF) {
+    fprintf(stderr, " at end");
+  } else if (token->type == TOKEN_ERROR) {
+    // Do Nothing
+  } else {
+    fprintf(stderr, " at '%.*s'", token->length, token->start);
+  }
+
+  fprintf(stderr, ": %s\n", message);
+  parser.hadError = true;
+}
+
+static void error(const char* message) {
+  errorAt(&parser.previous, message);
+}
+
+static void errorAtCurrent(const char* message) {
+  errorAt(&parser.current, message);
+}
+
+static void advance() {
+  parser.previous = parser.current;
+
   for (;;) {
-    Token token = scanToken();
-    if (token.line != line) {
-      printf("%4d ", token.line);
-    } else {
-      printf("   | ");
-    }
-
-    // '*' lets us pass precision as an argument - print the first token.length characters of the string at token.start
-    printf("%2d '%.*s'\n", token.type, token.length, token.start);
-
-    if (token.type == TOKEN_EOF) {
+    parser.current = scanToken();
+    if (parser.current.type != TOKEN_ERROR) {
       break;
     }
+
+    errorAtCurrent(parser.current.start);
   }
+}
+
+static void consume(TokenType type, const char* message) {
+  if (parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
+bool compile(const char* source, Chunk* chunk) {
+  initScanner(source);
+
+  parser.hadError = false;
+  parser.panicMode = false;
+
+  // 'primes the pump' on the scanner by loading a token into `previous` token
+  advance();
+  expression();
+  consume(TOKEN_EOF, "Expect end of expression.");
+  return !parser.hadError;
 }
