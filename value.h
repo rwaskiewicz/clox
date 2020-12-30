bff4651536bd657f663c13b8c0c9a20a83fd4912
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2020-12-30 15:34:04 -0500

    Thru 18.3 'Dynamically Typed Numbers'
    - Replace Value typedef with a struct containing:
        - A ValueTag, representing the type of a value
        - A union 'as' with a bool and number reference
    - Add ValueTag enum
    - Add macros for:
        - Checking the type of a Value
        - Converting clox values to C values
        - Converting C values to clox values
    - Update code (VM, compiler) to support new Value defintion
    - Add runtimeError reporting to the VM

--- value.h
+++ value.h
@@ -3,7 +3,36 @@
 
 #include "common.h"
 
-typedef double Value;
+// The 'tag' of the tagged union representing the type of a Value.
+// Note these are values supported by the VM, not the values that a user may
+// define themselves (e.g. a class)
+typedef enum {
+  VAL_BOOL,
+  VAL_NIL,
+  VAL_NUMBER,
+} ValueType;
+
+typedef struct {
+  ValueType type;
+  union {
+    bool boolean;
+    double number;
+  } as;
+} Value;
+
+// Convert a native C value to clox Value
+#define BOOL_VAL(value)   ((Value){VAL_BOOL, {.boolean = value}})
+#define NIL_VALUE         ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+
+// Convert a clox Value back to C
+#define AS_BOOL(value)   ((value).as.boolean)
+#define AS_NUMBER(value) ((value).as.number)
+
+// Check a value's type
+#define IS_BOOL(value)   ((value).type == VAL_BOOL)
+#define IS_NIL(value)    ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
 
 typedef struct {
   int capacity;
