4f605667357986938f81d54e181b2adcb8b7a263
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-02-21 17:08:56 -0500

    Through 22.2 'Block Statements'
    - Add initial support for block statements (as the name implies, blocks are statements
    - Increment and decrement the current compiler's scope depth each time we go in/out a block

--- compiler.c
+++ compiler.c
@@ -185,6 +185,14 @@ static void endCompiler() {
 #endif
 }
 
+static void beginScope() {
+  current->scopeDepth++;
+}
+
+static void endScope() {
+  current->scopeDepth--;
+}
+
 static void expression();
 static void statement();
 static void declaration();
@@ -436,6 +444,14 @@ static void expression() {
   parsePrecedence(PREC_ASSIGNMENT);
 }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+
+  consume(TOKEN_RIGHT_BRACE, "Expect '}' after block.");
+}
+
 static void varDeclaration() {
   // get the variable name
   uint8_t global = parseVariable("Expect variable name.");
@@ -510,6 +526,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
