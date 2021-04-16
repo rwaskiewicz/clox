4cb09d5698032feabdeca586d5b8f77072d598d2
Ryan Waskiewicz <ryanwaskiewicz@gmail.com>
2021-04-16 17:35:04 -0400

    Through 30.2 'Faster Hash Table Probing'
    - Speed up hash table probing by using bit masking over modulo

--- table.c
+++ table.c
@@ -28,7 +28,7 @@ void freeTable(Table* table) {
  * linear probing. Returns the address of the Entry to set
  */
 static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
-  uint32_t index = key->hash % capacity;
+  uint32_t index = key->hash & (capacity - 1);
   Entry* tombstone = NULL;
 
   // we won't loop forever thanks to the load factor
@@ -52,7 +52,7 @@ static Entry* findEntry(Entry* entries, int capacity, ObjString* key) {
     }
 
     // Case: We have a collision, start linear probing
-    index = (index + 1) % capacity;
+    index = (index + 1) & (capacity - 1);
   }
 }
 
@@ -155,7 +155,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
     return NULL;
   }
 
-  uint32_t index = hash % table->capacity;
+  uint32_t index = hash & (table->capacity - 1);
 
   for (;;) {
     Entry* entry = &table->entries[index];
@@ -171,7 +171,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
       // found it!
       return entry->key;
     }
-    index = (index + 1) % table->capacity;
+    index = (index + 1) & (table->capacity - 1);
   }
 }
 
